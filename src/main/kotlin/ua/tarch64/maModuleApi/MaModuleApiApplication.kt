4669f404c5cf133536400e33a8e63ad581e3f842
TArch64 <tarasturchenko1551@gmail.com>
2021-08-13 16:47:09 +0300

    init

---
+++ src/main/kotlin/ua/tarch64/maModuleApi/MaModuleApiApplication.kt
@@ -0,0 +1,11 @@
+package ua.tarch64.maModuleApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaModuleApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaModuleApiApplication>(*args)
+}
---
+++ src/test/kotlin/ua/tarch64/maModuleApi/MaModuleApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.tarch64.maModuleApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaModuleApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

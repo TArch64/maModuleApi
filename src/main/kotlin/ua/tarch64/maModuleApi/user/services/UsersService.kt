8d6a3ea6b777070b23536ac35db2a057591e5d31
TArch64 <tarasturchenko1551@gmail.com>
2021-09-23 19:56:04 +0300

    add sign-up validation

--- src/main/kotlin/ua/tarch64/maModuleApi/user/services/UsersService.kt
+++ src/main/kotlin/ua/tarch64/maModuleApi/user/services/UsersService.kt
@@ -2,6 +2,7 @@ package ua.tarch64.maModuleApi.user.services
 
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import ua.tarch64.maModuleApi.common.errorHandling.exceptions.ValidationException
 import ua.tarch64.maModuleApi.common.helpers.asKOptional
 import ua.tarch64.maModuleApi.user.entities.UserEntity
 import ua.tarch64.maModuleApi.user.entities.UserRepository
@@ -16,6 +17,10 @@ class UsersService(
     }
 
     fun createUser(options: UserEntity.CreateOptions): UserEntity {
+        if (findUserByUsername(options.username) != null) {
+            throw ValidationException("Username already exists")
+        }
+
         val user = UserEntity(
             0,
             options.role,
@@ -24,7 +29,6 @@ class UsersService(
         )
         return repository.save(user)
     }
-
     fun getUserById(userId: Long): UserEntity? {
         return repository.findById(userId).asKOptional()
     }
